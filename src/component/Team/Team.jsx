c03067fa1f060cb9f16de4f684e6c812ae15827f
Fazi <faf2001f@gmail.com>
2024-02-27 04:10:38 +0500

    ...

--- src/component/Team/Team.jsx
+++ src/component/Team/Team.jsx
@@ -9,45 +9,39 @@ export default function Team() {
       <div className={styles.teamMembers}>
         
         <div className={styles.teamMember}>
-          <img src="../../assets/Samantha Doe.jpg" alt="Samantha Doe" className={styles.memberImage} />
+          <img src="../../../assets/Image-1.png" alt="Yassine M." className={styles.memberImage} />
           <h3>Yassine M.</h3>
           <p className={styles.memberTitle}>Lead Developer</p>
           <p className={styles.memberDescription}>
           Yassine represents one of the key tech leads in our tutoring platform at Drexel University. He has outstanding development skills and a strong passion for new technologies. He really upgrades our educational offers.
           </p>
-          <div className={styles.socialMedia}>
-            <FaFacebookF />
-            <FaXing />
+          <a target='__black' href='https://www.linkedin.com/in/yassinemijane/' className={styles.socialMedia}>
             <FaLinkedinIn />
-          </div>
+          </a>
         </div>
         
         <div className={styles.teamMember}>
-          <img src="../../assets/Olivia Rodriguez.jpg" alt="Olivia Rodriguez" className={styles.memberImage} />
+          <img src="../../../assets/Image-3.png" alt="Ibrahim B." className={styles.memberImage} />
           <h3>Ibrahim B.</h3>
           <p className={styles.memberTitle}>Marketing Strategist</p>
           <p className={styles.memberDescription}>
           Ibrahim is studying for a Civil Engineering degree at Drexel University, and he is in charge of leading our marketing efforts with a strategic approach effectively expanding our reach and impact. 
           </p>
-          <div className={styles.socialMedia}>
-            <FaFacebookF />
-            <FaXing />
+          <a target='__black' href='https://www.linkedin.com/in/ibrahim-bassyouny-970641281?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app' className={styles.socialMedia}>
             <FaLinkedinIn />
-          </div>
+          </a>
         </div>
 
         <div className={styles.teamMember}>
-          <img src="../../assets/Marcus Douglas.jpg" alt="Marcus Douglas" className={styles.memberImage} />
-          <h3>William L.</h3>
+          <img src="../../../assets/Image-2.jpg" alt="William L." className={styles.memberImage} />
+          <h3>Willem L.</h3>
           <p className={styles.memberTitle}>Fundraising Coordinator</p>
           <p className={styles.memberDescription}>
           William is a Drexel University Finance major who is in charge of leading our financial strategy, with a focus on raising funds for our growth and also providing community engagement in advancing the organization and society.
           </p>
-          <div className={styles.socialMedia}>
-            <FaFacebookF />
-            <FaXing />
+          <a target='__black' href='https://www.linkedin.com/in/willem-lobley-97837829a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app' className={styles.socialMedia}>
             <FaLinkedinIn />
-          </div>
+          </a>
         </div>
 
       </div>
